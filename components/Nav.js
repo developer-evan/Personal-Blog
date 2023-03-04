6f8fcc850962cbbae14044f95643182e02bd9ae9
Evans-254 <71435810+Evans-254@users.noreply.github.com>
2023-03-04 12:59:19 +0300

    Personal Blog App

---
+++ components/Nav.js
@@ -0,0 +1,15 @@
+export default function Home() {
+    return (
+      <div className="flex justify-around mt-5 ">
+        <h1 className="text-3xl font-bold ">
+        Personal Blog
+      </h1>
+      <ul className="flex">
+        <li className="ml-8">Home</li>
+        <li className="ml-8">About Us</li>
+        <li className="ml-8">Services</li>
+        <li className="ml-8">Blog</li>
+      </ul>
+      </div>
+    )
+  }
