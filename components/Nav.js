16b5a34bfcb46e3069514f01cbf1ca16043f0b56
Evans-254 <71435810+Evans-254@users.noreply.github.com>
2023-03-04 13:09:58 +0300

    added styles to Nav

--- components/Nav.js
+++ components/Nav.js
@@ -5,11 +5,11 @@ export default function Home() {
         Personal Blog
       </h1>
       <ul className="flex">
-        <li className="ml-8 bg-green-500 p-2">Home</li>
-        <li className="ml-8">About Us</li>
-        <li className="ml-8">Services</li>
-        <li className="ml-8">Our Terms</li>
-        <li className="ml-8">Blog</li>
+        <li className="ml-8 bg-green-500 p-1">Home</li>
+        <li className="ml-8 bg-green-500 p-1">About Us</li>
+        <li className="ml-8 bg-green-500 p-1">Services</li>
+        <li className="ml-8 bg-green-500 p-1">Our Terms</li>
+        <li className="ml-8 bg-green-500 p-1">Blog</li>
       </ul>
 
       </div>
