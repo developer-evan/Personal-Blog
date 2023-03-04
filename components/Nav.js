5d1b70ac504cef36eb6b062df44fcfa1fb280ac1
Evans-254 <71435810+Evans-254@users.noreply.github.com>
2023-03-04 13:19:54 +0300

    Added styles to Nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,15 +1,15 @@
 export default function Home() {
     return (
       <div className="flex justify-around mt-5 ">
-        <h1 className="text-3xl font-bold ">
+        <h1 className="text-3xl font-bold text-green-500 ">
         Personal Blog
       </h1>
       <ul className="flex">
-        <li className="ml-8 bg-green-500 p-1">Home</li>
-        <li className="ml-8 bg-green-500 p-1">About Us</li>
-        <li className="ml-8 bg-green-500 p-1">Services</li>
-        <li className="ml-8 bg-green-500 p-1">Our Terms</li>
-        <li className="ml-8 bg-green-500 p-1">Blog</li>
+        <li className="ml-8 font-bold  text-green-500 p-1">Home</li>
+        <li className="ml-8 font-bold text-green-500 p-1">About Us</li>
+        <li className="ml-8 font-bold text-green-500 p-1">Services</li>
+        <li className="ml-8 font-bold text-green-500 p-1">Our Terms</li>
+        <li className="ml-8 font-bold text-green-500 p-1">Blog</li>
       </ul>
 
       </div>
